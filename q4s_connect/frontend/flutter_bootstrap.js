8b87547ab915d2c254b16b70b8fb4f5d76534f2c
OsamaR <osama.hon98@gmail.com>
2026-06-24 12:21:28 +0300

    updated frontend

--- q4s_connect/frontend/flutter_bootstrap.js
+++ q4s_connect/frontend/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"d2913632a4578ee4d0b8b1c4a69888c8a0672c
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "505165419"
+    serviceWorkerVersion: "4224449654"
   }
 });
